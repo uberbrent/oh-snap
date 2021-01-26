8491428354a5aaaf45f16484b0cdbbd36b965c40
uberbrent <bmjohns1337@gmail.com>
2021-01-26 09:29:15 -0600

    updates

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
+import Gallery from './components/Gallery';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
--- photo-port/src/components/Nav/index.js
+++ photo-port/src/components/Nav/index.js
@@ -1,27 +1,31 @@
-import React from 'react';
-const categories = [
-    {
-        name: "commercial",
-        description: "Photos of grocery stores, food trucks, and other commercial projects",
-    },
-    { name: "portraits", description: "Portraits of people in my life" },
-    { name: "food", description: "Delicious delicacies" },
-    {
-        name: "landscape",
-        description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-    },
-];
-
-function categorySelected(name) {
-    console.log(`${name} clicked`)
-}
+import React, { useState } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav() {
+    const [categories] = useState([
+        {
+            name: "commercial",
+            description: "Photos of grocery stores, food trucks, and other commercial projects",
+        },
+        { name: "portraits", description: "Portraits of people in my life" },
+        { name: "food", description: "Delicious delicacies" },
+        {
+            name: "landscape",
+            description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+        },
+    ]);
+    
+    const [currentCategory, setCurrentCategory] = useState(categories[0]);
+    
     return (
-        <header>
+        <header className="flex-row px-1">
             <h2>
                 <a data-testid="link" href='/'>
-                    <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+                    <span role="img" aria-label="camera">
+                        {" "} 
+                        📸
+                        </span>{" "}
+                         Oh Snap!
                 </a>
             </h2>
             <nav>
@@ -34,12 +38,13 @@ function Nav() {
                         <span>contact</span>
                     </li>
                     {categories.map((category) => (
-                    <li
-                        className="mx-1"
+                    <li className={`mx-1 ${
+                        currentCategory.name === category.name && 'navActive'
+                    }`} 
                         key={category.name}
                     >
-                        <span onClick={() => categorySelected(category.name)} >
-                            {category.name}
+                        <span onClick={() => setCurrentCategory(category)} >
+                            {capitalizeFirstLetter(category.name)}
                         </span>
                     </li>
                     ))}
