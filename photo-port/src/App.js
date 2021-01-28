e183b4e20151fcaeef8bcabc1c197c6d4a1a4015
uberbrent <bmjohns1337@gmail.com>
2021-01-28 10:38:09 -0600

    added testing and Contact component testing

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -13,8 +14,9 @@ function App() {
     { name: 'food', description: 'Delicious delicacies' },
     { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
   ]);
-
+  
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
@@ -22,10 +24,18 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
--- photo-port/src/components/Contact/index.js
+++ photo-port/src/components/Contact/index.js
@@ -1,23 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: ''});
+
+    const [errorMessage, setErrorMessage] = useState('')
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        if(e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            if(!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                if (!e.target.value.length) {
+                    setErrorMessage(`${e.target.name} is required.`);
+                } else {
+                    setErrorMessage('');
+                }
+                console.log('errorMessage', errorMessage)
+            }
+        }
+        if(!errorMessage) {
+        setFormState({...formState, [e.target.name]: e.target.value})
+        }
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        validateEmail(formState)
+        console.log(formState)
+    }
+
     return(
         <section>
-            <h1>Contact Me</h1>
-            <form id="contact-form">
+            <h1 data-testid="h1tag">Contact Me</h1>
+            <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" name="name" onBlur={handleChange} defaultValue={name} />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" name="email" onBlur={handleChange} defaultValue={email} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <input type="message" rows="5" />
-                </div> 
-                <button type="submit">Submit</button>
+                    <textarea name="message" rows="5" onBlur={handleChange} defaultValue={message} />
+                </div>
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )} 
+                <button type="submit" data-testid="buttontag">Submit</button>
             </form>
         </section>
     )
